473e91ad810cf76099316331f3d0bd5f3df8891a
PyXiion <me@pyxiion.ru>
2023-07-24 10:15:04 +0300

    Парс аргументов

--- src/engine/general.hpp
+++ src/engine/general.hpp
@@ -1,11 +1,16 @@
 #pragma once
+#include <string>
 #include <string_view>
+
+#define PX_ENGINE_VERSION "0.0.1"
 
 namespace px::info
 {
-  const int engineMajorVersion = 0;
-  const int engineMinorVersion = 0;
-  const int enginePatchVersion = 1;
+  constexpr int engineMajorVersion = 0;
+  constexpr int engineMinorVersion = 0;
+  constexpr int enginePatchVersion = 1;
+  
+  constexpr std::string_view engineVersion = PX_ENGINE_VERSION;
 
   constexpr std::string_view engineBuildDateTime = __DATE__ " " __TIME__;
 
--- src/game/main.cpp
+++ src/game/main.cpp
@@ -1,17 +1,51 @@
 #include <iostream>
 
 #include <easy/profiler.h>
-#include "../engine/engine.hpp"
-#include "../engine/system/general.hpp"
+#include <args.hxx>
+#include <engine/general.hpp>
+#include <fmt/format.h>
+#include "game.hpp"
 
-int main()
+const std::string argsDesc = "PXE - PyXiion's Engine v" PX_ENGINE_VERSION;
+const std::string argsEpilog = "";
+
+int main(int argc, char **argv)
 {
-  EASY_PROFILER_ENABLE;
+  args::ArgumentParser parser(argsDesc, argsEpilog);
+  args::HelpFlag help(parser, "help", "Показать это меню", {'h', "help"});
+  args::CompletionFlag completion(parser, {"complete"});
+
+  args::ValueFlag<std::string> profilerOut(parser, "profilerOut", "Выходной файл профайлера", {'p', "profiler"});
+  
+  try
+  {
+    parser.ParseCLI(argc, argv);
+  }
+  catch (const args::Completion &e)
+  {
+    std::cout << e.what();
+    return EXIT_SUCCESS;
+  }
+  catch (const args::Help &)
+  {
+    std::cout << parser;
+    return EXIT_SUCCESS;
+  }
+  catch (const args::ParseError &e)
+  {
+    std::cerr << e.what() << std::endl;
+    std::cerr << parser;
+    return EXIT_FAILURE;
+  }
+
+  profiler::setEnabled(profilerOut);
 
   px::Engine engine;
+  px::Game game(engine);
 
   engine.run();
 
-  profiler::dumpBlocksToFile("debug_profile.prof");
+  if (profiler::isEnabled())
+    profiler::dumpBlocksToFile(args::get(profilerOut).c_str());
   return EXIT_SUCCESS;
 }
