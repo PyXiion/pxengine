1930e919077207481923b28feedd789497245d25
PyXiion <me@pyxiion.ru>
2023-08-07 19:39:55 +0300

    Комментари и исправления

--- src/engine/common/dynamic_enum.cpp
+++ src/engine/common/dynamic_enum.cpp
@@ -1,4 +1,6 @@
 #include "dynamic_enum.hpp"
+#include <algorithm>
+#include <stdexcept>
 
 px::DynamicEnum::EnumValue px::DynamicEnum::add(const std::string& key)
 {
@@ -17,10 +19,20 @@ px::DynamicEnum::EnumValue px::DynamicEnum::claimValue()
 
 px::DynamicEnum::EnumValue px::DynamicEnum::get(const std::string& key) const
 {
-  return m_values.at(key);
+  auto it = m_values.find(key);
+  if (it == m_values.end())
+  {
+    throw std::out_of_range("Ключ перечисления не найден.");
+  }
+  return it->second;
 }
 
 std::string px::DynamicEnum::get(px::DynamicEnum::EnumValue val) const
 {
-  return m_keys.at(val);
+  auto it = m_keys.find(val);
+  if (it == m_keys.end())
+  {
+    throw std::out_of_range("Значение перечисления не найдено.");
+  }
+  return it->second;
 }
--- src/engine/common/dynamic_enum.hpp
+++ src/engine/common/dynamic_enum.hpp
@@ -4,21 +4,49 @@
 
 namespace px
 {
+  /**
+   * @brief Класс для создания динамического перечисления.
+   */
   class DynamicEnum
   {
   public:
-    using EnumValue = int32_t;
+    using EnumValue = int;
 
-  public:
+    /**
+     * @brief Конструктор по умолчанию.
+     */
+    DynamicEnum() = default;
+
+    /**
+     * @brief Добавить новый элемент в перечисление.
+     * @param key Ключ элемента.
+     * @return Значение элемента.
+     */
     EnumValue add(const std::string &key);
 
+    /**
+     * @brief Получает значение элемента по его ключу.
+     * @param key Ключ элемента.
+     * @return Значение элемента.
+     * @throw std::out_of_range если элемент с данным ключом отсуствует в перечислении.
+     */
     EnumValue get(const std::string &key) const;
+
+    /**
+     * @brief Получает ключ элемента по его значению.
+     * @param val Значение элемента.
+     * @return Ключ элемента.
+     * @throw std::out_of_range если элемент с данным значением отсутствует в перечислении.
+     */
     std::string get(EnumValue val) const;
 
   private:
     std::unordered_map<std::string, EnumValue> m_values;
     std::unordered_map<EnumValue, std::string> m_keys;
 
+    /**
+     * @brief Получить следующее свободное значение элемента. 
+     */
     EnumValue claimValue();
   };
 }
