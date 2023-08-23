3b459ffb1006b9ba37792176f595c1b360acffb8
PyXiion <me@pyxiion.ru>
2023-08-23 19:44:58 +0300

    Allow no limit for FPS

--- src/engine/common/frame_limiter.cpp
+++ src/engine/common/frame_limiter.cpp
@@ -1,22 +1,26 @@
 #include "frame_limiter.hpp"
 #include <thread>
 
-
-
 px::FrameLimiter::FrameLimiter(int maxFps)
   : m_clock()
-  , m_frameDelta(std::chrono::microseconds(std::chrono::seconds(1)) / maxFps)
   , m_lastFrame(m_clock.now())
   , m_target(m_lastFrame)
 {
+  if (maxFps != 0.0f)
+    m_frameDelta = std::chrono::microseconds(std::chrono::seconds(1)) / maxFps;
+  else
+    m_frameDelta = std::chrono::seconds(0);
 }
 
 float px::FrameLimiter::sleep()
 {
-  std::this_thread::sleep_until(m_target);
   auto now = m_clock.now();
-
-  m_target = now + m_frameDelta;
+  
+  if (m_frameDelta != std::chrono::seconds(0))
+  {
+    std::this_thread::sleep_until(m_target);
+    m_target = now + m_frameDelta;
+  }
 
   using ms = std::chrono::duration<float, std::milli>;
   float deltaTime = std::chrono::duration_cast<ms>(now - m_lastFrame).count() / 1000.0f;
