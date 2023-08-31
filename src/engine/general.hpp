38c029d66c28b596d7ef21ea13f2b88b0374865b
PyXiion <me@pyxiion.ru>
2023-08-31 15:11:12 +0300

    Rework general.hpp

--- src/engine/general.hpp
+++ src/engine/general.hpp
@@ -2,19 +2,23 @@
 #include <string>
 #include <string_view>
 
+#define PX_ENGINE_VERSION_MAJOR 0
+#define PX_ENGINE_VERSION_MINOR 0
+#define PX_ENGINE_VERSION_PATCH 1
 #define PX_ENGINE_VERSION "0.0.1"
 
 namespace px::info
 {
-  constexpr int engineMajorVersion = 0;
-  constexpr int engineMinorVersion = 0;
-  constexpr int enginePatchVersion = 1;
+  constexpr uint16_t engineMajorVersion = PX_ENGINE_VERSION_MAJOR;
+  constexpr uint16_t engineMinorVersion = PX_ENGINE_VERSION_MINOR;
+  constexpr uint16_t enginePatchVersion = PX_ENGINE_VERSION_PATCH;
+  constexpr uint16_t engineBuild = 1;
   
-  constexpr std::string_view engineVersion = PX_ENGINE_VERSION;
+  inline std::string engineVersion = PX_ENGINE_VERSION;
 
-  constexpr std::string_view engineBuildDateTime = __DATE__ " " __TIME__;
+  inline std::string engineBuildDateTime = __DATE__ " " __TIME__;
 
-  constexpr std::string_view engineWebUrl = "https://engine.pyxiion.ru/";
+  inline std::string engineWebUrl = "https://engine.pyxiion.ru/";
 
-  constexpr std::string_view glslVersion = "#version 400";
+  inline std::string glslVersion = "#version 400";
 }
