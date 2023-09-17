a9d19d0c019256e4315452d7077091b4cf02b006
PyXiion <me@pyxiion.ru>
2023-09-17 15:35:05 +0300

    CWD param

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <filesystem>
 
 #include <easy/profiler.h>
 #include <args.hxx>
@@ -6,8 +7,10 @@
 #include <fmt/format.h>
 #include "game.hpp"
 
+namespace fs = std::filesystem;
+
 const std::string argsDesc = "PXE - PyXiion's Engine v" PX_ENGINE_VERSION;
-const std::string argsEpilog = "";
+const std::string argsEpilog = "PyXiion (C) 2023";
 
 int main(int argc, char **argv)
 {
@@ -16,6 +19,7 @@ int main(int argc, char **argv)
   args::CompletionFlag completion(parser, {"complete"});
 
   args::ValueFlag<std::string> profilerOut(parser, "profilerOut", "Выходной файл профайлера", {'p', "profiler"});
+  args::ValueFlag<std::string> cwdPath(parser, "cwd", "Рабочая директория игры", {'c', "cwd"}, "./");
   
   try
   {
@@ -37,6 +41,8 @@ int main(int argc, char **argv)
     std::cerr << parser;
     return EXIT_FAILURE;
   }
+
+  fs::current_path(args::get(cwdPath));
 
   profiler::setEnabled(profilerOut);
 
