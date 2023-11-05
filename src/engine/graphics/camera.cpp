504cae40453d3bb7171e6df6e30e5f58532ee085
PyXiion <me@pyxiion.ru>
2023-11-05 13:43:54 +0300

    Create Camera

---
+++ src/engine/graphics/camera.cpp
@@ -0,0 +1,98 @@
+// Copyright (c) 2023.
+
+//
+// Created by pyxiion on 04.11.23.
+//
+
+#include "camera.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "../engine.hpp"
+#include "../world/world.hpp"
+
+namespace px {
+  Camera::Camera()
+    : m_renderer(getWorld().getEngine().getRenderer()) {
+
+  }
+
+  void Camera::perspective(float fovy, float aspect, float near, float far) {
+    setProjection(glm::perspective(glm::radians(fovy), aspect, near, far));
+  }
+
+  void Camera::setProjection(Matrix4x4 projection) {
+    m_proj = projection;
+  }
+
+  Matrix4x4 Camera::getProjection() const {
+    return m_proj;
+  }
+
+  void Camera::lookAt(Vector3 pos) {
+    m_direction = glm::normalize(m_position - pos);
+    m_changed = true;
+  }
+
+  void Camera::lookAt(float x, float y, float z) {
+    lookAt({x, y, z});
+  }
+
+  void Camera::setPosition(Vector3 pos) {
+    m_position = pos;
+    m_changed = true;
+  }
+
+  void Camera::setPosition(float x, float y, float z) {
+    setPosition({x, y, z});
+  }
+
+  Vector3 Camera::getPosition() const {
+    return m_position;
+  }
+
+  void Camera::move(Vector3 offset) {
+    m_position += offset;
+    m_changed = true;
+  }
+
+  void Camera::move(float offsetX, float offsetY, float offsetZ) {
+    move({offsetX, offsetY, offsetZ});
+  }
+
+  void Camera::apply() {
+    if (m_changed)
+      recalculate();
+
+    bgfx::setViewTransform(m_renderer.getViewId(), glm::value_ptr(m_view), glm::value_ptr(m_proj));
+
+    auto size = m_renderer.getFramebufferSize();
+    bgfx::setViewRect(m_renderer.getViewId(), 0, 0, uint16_t(size.x), uint16_t(size.y) );
+  }
+
+  void Camera::recalculate() {
+    Vector3 up = Vector3Up;
+    m_right = glm::normalize(glm::cross(up, m_direction));
+    m_up = glm::cross(m_direction, m_right);
+    m_view = glm::lookAt(m_position, m_position + m_direction, m_up);
+    m_changed = false;
+  }
+
+  Vector3 Camera::getUp() {
+    if (m_changed)
+      recalculate();
+    return m_up;
+  }
+
+  Vector3 Camera::getRight() {
+    if (m_changed)
+      recalculate();
+    return m_right;
+  }
+
+  Vector3 Camera::getForward() {
+    if (m_changed)
+      recalculate();
+    return m_direction;
+  }
+} // px
--- src/engine/graphics/camera.hpp
+++ src/engine/graphics/camera.hpp
@@ -1,27 +1,62 @@
-#pragma once
-#include <glm/glm.hpp>
-#include "../math/vector.hpp"
+// Copyright (c) 2023.
+
+//
+// Created by pyxiion on 04.11.23.
+//
+
+#ifndef ENGINE_CAMERA_HPP
+#define ENGINE_CAMERA_HPP
+#include "renderer.hpp"
 #include "../math/matrix.hpp"
+#include "../math/vector.hpp"
+#include "../world/game_object.hpp"
 
-namespace px
-{
-  class Camera
-  {
+namespace px {
+  class Camera : public GameObject {
   public:
-    Camera();
+    explicit Camera();
 
-    void createPerspectiveProjectionMatrix(float FoV, float aspect, float near = 0.1f, float far = 100.0f);
+    void perspective(float fovy, float aspect, float near = 0.1f, float far = 100.0f);
+    void setProjection(Matrix4x4 projection);
+    [[nodiscard]] Matrix4x4 getProjection() const;
 
-    void rotateAt(Vector3 target, Vector3 up = Vector3Up);
+    void setPosition(Vector3 pos);
+    void setPosition(float x, float y, float z);
+    [[nodiscard]] Vector3 getPosition() const;
 
-    const Matrix4x4 &getProjectionMatrix();
-    const Matrix4x4 &getViewMatrix();
+    void lookAt(Vector3 pos);
+    void lookAt(float x, float y, float z);
+
+    void move(Vector3 offset);
+    void move(float offsetX, float offsetY, float offsetZ);
+
+    Vector3 getUp();
+    Vector3 getRight();
+    Vector3 getForward();
+
+    void apply();
 
   private:
-    float m_pitch;
-    float m_yaw;
+    Renderer &m_renderer;
 
-    Matrix4x4 m_projection;
-    Matrix4x4 m_view;
+    px::Vector3 m_position{};
+    px::Vector3 m_rotation{}; ///< euler rotation
+    px::Vector3 m_direction{};
+
+    px::Vector3 m_up{};
+    px::Vector3 m_right{};
+
+    px::Matrix4x4 m_view{};
+    px::Matrix4x4 m_proj{};
+
+    bool m_anglesChanged = true;
+    bool m_changed = true;
+
+    void recalculate();
   };
-}
+
+  typedef std::shared_ptr<Camera> CameraPtr;
+
+} // px
+
+#endif //ENGINE_CAMERA_HPP
