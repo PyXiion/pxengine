3eed49163e98897ade0fd5f73bbcde9ab1e00b4d
PyXiion <me@pyxiion.ru>
2023-11-10 22:46:46 +0300

    ImGui bgfx utils

--- src/engine/utils/bgfx_utils.hpp
+++ src/engine/utils/bgfx_utils.hpp
@@ -9,6 +9,13 @@
 
 namespace px::utils::bgfx {
   extern const ::bgfx::Memory *stringToMemory(const std::string &str);
+
+  inline bool checkAvailTransientBuffers(uint32_t _numVertices, const ::bgfx::VertexLayout& _layout, uint32_t _numIndices)
+  {
+    return _numVertices == ::bgfx::getAvailTransientVertexBuffer(_numVertices, _layout)
+           && (0 == _numIndices || _numIndices == ::bgfx::getAvailTransientIndexBuffer(_numIndices) )
+        ;
+  }
 }
 
 #endif //ENGINE_BGFX_UTILS_HPP
