e358364eb20ff85a677682e973f3bedc8ae38ee8
PyXiion <me@pyxiion.ru>
2023-11-10 22:48:14 +0300

    Better resource management

--- src/engine/resources/resource_manager.cpp
+++ src/engine/resources/resource_manager.cpp
@@ -1,19 +1,110 @@
 #include "resource_manager.hpp"
 #include <filesystem>
+#include <px/string_utils.hpp>
+#include <px/visit_overloaded.hpp>
+#include <utility>
+#include <fstream>
 #include "../common/utils.hpp"
 #include "../engine.hpp"
 #include "../utils/bgfx_utils.hpp"
 
 namespace fs = std::filesystem;
 
-px::ResourceManager::ResourceManager(Engine &engine, const std::string &rootDir)
+px::ResourceManager::ResourceManager(Engine &engine, std::string rootDir)
   : m_engine(engine)
-  , m_rootDir(rootDir)
+  , m_rootDir(std::move(rootDir))
 {
 
 }
 
-std::string px::ResourceManager::normalizePath(const std::string &_path)
+px::ResourceManager::~ResourceManager() {
+}
+
+bgfx::ProgramHandle px::ResourceManager::loadProgram(const std::string &vsName, const std::string &fsName, bool reload) {
+  std::string key = vsName + "," + fsName;
+
+  Resource &resource = (*this)[key];
+  if (not reload and std::holds_alternative<BgfxUniqueProgramHandle>(resource)) {
+    return static_cast<bgfx::ProgramHandle>(std::get<BgfxUniqueProgramHandle>(resource));
+  }
+
+  bgfx::ShaderHandle vsh = loadShader(vsName, reload);
+  bgfx::ShaderHandle fsh = BGFX_INVALID_HANDLE;
+  if (!fsName.empty())
+  {
+    fsh = loadShader(fsName, reload);
+  }
+
+  auto program = bgfx::createProgram(vsh, fsh, false /* destroy shaders when program is destroyed */);
+  resource = BgfxUniqueProgramHandle(program);
+  return program;
+}
+
+bgfx::ShaderHandle px::ResourceManager::loadShader(const std::string &filename, bool reload) {
+  Resource &resource = (*this)[filename];
+  if (not reload and std::holds_alternative<BgfxUniqueShaderHandle>(resource)) {
+    return static_cast<bgfx::ShaderHandle>(std::get<BgfxUniqueShaderHandle>(resource));
+  }
+
+  // Create a new shader if not cached
+  std::vector<std::string> shaderPath = ResourceManager::parseResourcePath(filename);
+  std::string shaderType;
+
+  switch (bgfx::getRendererType()) {
+    case bgfx::RendererType::Noop:
+    case bgfx::RendererType::Direct3D9:  shaderType = "dx9"; break;
+    case bgfx::RendererType::Direct3D11:
+    case bgfx::RendererType::Direct3D12: shaderType = "dx11";  break;
+    case bgfx::RendererType::Agc:
+    case bgfx::RendererType::Gnm:        shaderType = "pssl";  break;
+    case bgfx::RendererType::Metal:      shaderType = "metal"; break;
+    case bgfx::RendererType::Nvn:        shaderType = "nvn";   break;
+    case bgfx::RendererType::OpenGL:     shaderType = "glsl";  break;
+    case bgfx::RendererType::OpenGLES:   shaderType = "essl";  break;
+    case bgfx::RendererType::Vulkan:
+    case bgfx::RendererType::WebGPU:     shaderType = "spirv"; break;
+
+    default:
+      throw std::runtime_error("Unknown renderer type.");
+  }
+  shaderPath.insert(shaderPath.end() - 1, shaderType);
+  fs::path path = m_rootDir;
+  for (const auto& i : shaderPath)
+    path /= i;
+
+  path.replace_extension(".bin");
+
+  bgfx::ShaderHandle handle = bgfx::createShader(utils::bgfx::stringToMemory(px::io::loadFileToString(path)));
+  bgfx::setName(handle, filename.c_str());
+
+  resource = BgfxUniqueShaderHandle(handle);
+  return handle;
+}
+
+px::ResourceManager::RawType px::ResourceManager::loadRawFile(const std::string &_path, bool reload) {
+  fs::path path = absolutePath(_path);
+
+  Resource &resource = (*this)[_path];
+  if (not reload and std::holds_alternative<RawTypeHandler>(resource)) {
+    auto &handle = std::get<RawTypeHandler>(resource);
+    return RawType(handle.data(), handle.size());
+  }
+
+  std::ifstream ifs(path, std::ios::in | std::ios::binary);
+  if (not ifs.is_open()) {
+    throw std::runtime_error("File does not exist.");
+  }
+  px::MemoryStream ms;
+  ifs >> ms;
+  ifs.close();
+
+  RawType raw(ms.data(), ms.size());
+  resource = std::move(ms);
+
+  return raw;
+}
+
+std::string px::ResourceManager::absolutePath(const std::string &_path)
 {
   fs::path path = _path;
   if (path.is_absolute())
@@ -24,41 +115,10 @@ std::string px::ResourceManager::normalizePath(const std::string &_path)
   return fs::absolute(fs::path(m_rootDir) / path);
 }
 
-bgfx::ProgramHandle px::ResourceManager::loadProgram(const std::string &vsName, const std::string &fsName) {
-  bgfx::ShaderHandle vsh = loadShader(vsName);
-  bgfx::ShaderHandle fsh = BGFX_INVALID_HANDLE;
-  if (!fsName.empty())
-  {
-    fsh = loadShader(fsName);
-  }
-
-  return bgfx::createProgram(vsh, fsh, true /* destroy shaders when program is destroyed */);
+std::vector<std::string> px::ResourceManager::parseResourcePath(const std::string &path) {
+  return px::split(path, ".");
 }
 
-bgfx::ShaderHandle px::ResourceManager::loadShader(const std::string &filename) {
-  auto shaderPath = fs::path(m_rootDir) / "core/shaders";
-  switch (bgfx::getRendererType()) {
-    case bgfx::RendererType::Noop:
-    case bgfx::RendererType::Direct3D9:  shaderPath /= "dx9"; break;
-    case bgfx::RendererType::Direct3D11:
-    case bgfx::RendererType::Direct3D12: shaderPath /= "dx11";  break;
-    case bgfx::RendererType::Agc:
-    case bgfx::RendererType::Gnm:        shaderPath /= "pssl";  break;
-    case bgfx::RendererType::Metal:      shaderPath /= "metal"; break;
-    case bgfx::RendererType::Nvn:        shaderPath /= "nvn";   break;
-    case bgfx::RendererType::OpenGL:     shaderPath /= "glsl";  break;
-    case bgfx::RendererType::OpenGLES:   shaderPath /= "essl";  break;
-    case bgfx::RendererType::Vulkan:
-    case bgfx::RendererType::WebGPU:     shaderPath /= "spirv/"; break;
-
-    case bgfx::RendererType::Count:
-      assert(false), "You should not be here!";
-      break;
-  }
-
-  shaderPath /= filename + ".bin";
-
-  bgfx::ShaderHandle handle = bgfx::createShader(utils::bgfx::stringToMemory(px::io::loadFileToString(shaderPath)));
-  bgfx::setName(handle, filename.c_str());
-  return handle;
+px::ResourceManager::Resource &px::ResourceManager::operator[](const std::string &key) {
+  return m_cached[key];
 }
--- src/engine/resources/resource_manager.hpp
+++ src/engine/resources/resource_manager.hpp
@@ -2,6 +2,12 @@
 #include <string>
 #include <unordered_map>
 #include <bgfx/bgfx.h>
+#include <vector>
+#include <variant>
+#include <px/hash_utils.hpp>
+#include <px/raw_memory.hpp>
+#include <px/memory_stream.hpp>
+#include "bgfx_handle.hpp"
 
 namespace px
 { 
@@ -9,26 +15,45 @@ namespace px
   class ResourceManager
   {
   public:
-    ResourceManager(Engine &engine, const std::string &rootDir);
+    struct None {};
+    using RawType = px::RawConstMemory<char>;
+    using RawTypeHandler = px::MemoryStream;
+    using Resource = std::variant<
+        None,
+        BgfxUniqueVertexBufferHandle, BgfxUniqueIndexBufferHandle,
+        BgfxUniqueShaderHandle, BgfxUniqueProgramHandle,
+        RawTypeHandler>;
+
+  public:
+    ResourceManager(Engine &engine, std::string rootDir);
+    ~ResourceManager();
+
+    Resource &operator[](const std::string &key);
 
     /// Loads a shader from a file.
     /// \param filename The name of the file without a pre-directory. It will be determined automatically depending on the current graphics API.
     /// \return A bgfx shader handle
-    bgfx::ShaderHandle loadShader(const std::string &filename);
+    bgfx::ShaderHandle loadShader(const std::string &filename, bool reload = false);
 
     /// Loads a shader program from files.
     /// \param vsName The file name of the vertex shader.
     /// \param fsName The file name of the fragment shader.
     /// \return A bgfx program handle
-    bgfx::ProgramHandle loadProgram(const std::string &vsName, const std::string &fsName);
+    bgfx::ProgramHandle loadProgram(const std::string &vsName, const std::string &fsName, bool reload = false);
+
+    RawType loadRawFile(const std::string &path, bool reload = false);
 
   private:
     Engine &m_engine;
+
     std::string m_rootDir;
 
+    std::unordered_map<std::string, Resource> m_cached;
     /// Makes the path absolute depending on the root directory of the resources.
-    /// \param path A path.
+    /// \param _path A path.
     /// \return An absolute path.
-    std::string normalizePath(const std::string &path);
+    std::string absolutePath(const std::string &_path);
+
+    static std::vector<std::string> parseResourcePath(const std::string &path);
   };
 } // namespace px
