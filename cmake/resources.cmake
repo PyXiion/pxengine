80609151d6cde27cba516c311f59e90a2e3a89ee
PyXiion <me@pyxiion.ru>
2023-11-27 15:51:34 +0300

    Delete resources.cmake

--- cmake/resources.cmake
+++
@@ -1,5 +0,0 @@
-function(copy_resource_folder TARGET)
-  add_custom_command(TARGET ${TARGET} PRE_BUILD
-    COMMAND ${CMAKE_COMMAND} -E copy_directory
-    ${CMAKE_SOURCE_DIR}/data/ $<TARGET_FILE_DIR:${TARGET}>/data/)
-endfunction()
