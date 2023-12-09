8a414baa06148a5573de1db1b7e8bbdf52aeab52
PyXiion <me@pyxiion.ru>
2023-12-09 22:58:53 +0300

    Update render state

--- src/px/engine/graphics/render_states.hpp
+++ src/px/engine/graphics/render_states.hpp
@@ -13,8 +13,18 @@
 namespace px {
 
   struct RenderStates {
+    inline static constexpr uint64_t defaultState =
+          BGFX_STATE_WRITE_RGB
+        | BGFX_STATE_WRITE_A
+        | BGFX_STATE_WRITE_Z
+        | BGFX_STATE_DEPTH_TEST_LESS
+        | BGFX_STATE_CULL_CW
+        | BGFX_STATE_MSAA;
+
     ShaderPtr shaderPtr;
     bgfx::ViewId viewId{};
+
+    uint64_t state = defaultState;
   };
 
 } // px
