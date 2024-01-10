f13803f15e94fa22e920d8eed593bbd313f97525
PyXiion <me@pyxiion.ru>
2024-01-10 14:31:56 +0300

    AS registering exceptions

--- src/px/script/exceptions.hpp
+++ src/px/script/exceptions.hpp
@@ -12,9 +12,31 @@ namespace px::script {
   PX_NEW_EXCEPTION(NotFound, Exception);
 
   PX_NEW_EXCEPTION(FunctionNotFound, NotFound);
+
   PX_NEW_EXCEPTION(FactoryNotFound, NotFound);
+
   PX_NEW_EXCEPTION(MethodNotFound, NotFound);
+
   PX_NEW_EXCEPTION(PropertyNotFound, NotFound);
+
+
+  PX_NEW_EXCEPTION(RegisterError, Exception);
+
+  PX_NEW_EXCEPTION(RegisterClassError, RegisterError);
+
+  PX_NEW_EXCEPTION(RegisterClassTypeError, RegisterClassError);
+
+  PX_NEW_EXCEPTION(RegisterClassBehaviourError, RegisterClassError);
+
+  PX_NEW_EXCEPTION(RegisterClassMethodError, RegisterClassError);
+
+  PX_NEW_EXCEPTION(RegisterClassProxyMethodError, RegisterClassMethodError);
+
+  PX_NEW_EXCEPTION(RegisterClassCdeclMethodError, RegisterClassMethodError);
+
+  PX_NEW_EXCEPTION(RegisterClassGenericMethodError, RegisterClassMethodError);
+
+  PX_NEW_EXCEPTION(RegisterClassPropertyError, RegisterClassError);
 }
 
 #endif //PX_SCRIPT_EXCEPTIONS_HPP
