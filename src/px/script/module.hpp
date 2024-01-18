52582f74dbf3a6b2f711b43f2036cdf7c9cc4ddc
PyXiion <me@pyxiion.ru>
2024-01-18 13:01:35 +0300

    Split template function

--- src/px/script/module.hpp
+++ src/px/script/module.hpp
@@ -21,23 +21,15 @@ namespace px::script {
     ~Module();
 
     template<FunctionSignature T>
-    auto getFunction(const std::string &funcName) {
-      const std::string decl = getSignature<T>(funcName);
+    auto getFunction(const std::string &funcName);
 
-      asIScriptFunction *funcHandle = getFunctionByDecl(decl);
+    ObjectType getType(const std::string &typeName);
 
-      if (not funcHandle) {
-        PX_THROW_AND_LOG("AngelScript", FunctionNotFound,
-                         "Failed to get function {} by declaration \"{}\"", funcName, decl);
-      }
+    ObjectType getTypeByIndex(unsigned int index);
 
-      CVLOG(1, "AngelScript") << "Requested AngelScript function by declaration " << decl;
-      return priv::GetFunctionType<T>(priv::FunctionHandle(m_ctx, funcHandle));
-    }
+    [[nodiscard]] unsigned int getObjectTypeCount() const;
 
-    auto getType(const std::string &typeName) {
-      return ObjectType(typeName, m_ctx, m_module);
-    }
+    void bindImported();
 
   private:
     asIScriptContext *m_ctx;
@@ -45,6 +37,21 @@ namespace px::script {
 
     asIScriptFunction *getFunctionByDecl(const std::string &funcDecl);
   };
+
+  template<FunctionSignature T>
+  auto Module::getFunction(const std::string &funcName) {
+    const std::string decl = getSignature<T>(funcName);
+
+    asIScriptFunction *funcHandle = getFunctionByDecl(decl);
+
+    if (not funcHandle) {
+      PX_THROW_AND_LOG("AngelScript", FunctionNotFound,
+                       "Failed to get function {} by declaration \"{}\"", funcName, decl);
+    }
+
+    CVLOG(1, "AngelScript") << "Requested AngelScript function by declaration " << decl;
+    return priv::GetFunctionType<T>(priv::FunctionHandle(m_ctx, funcHandle));
+  }
 } // px::script
 
 #endif //PX_ENGINE_SCRIPT_MODULE_HPP
