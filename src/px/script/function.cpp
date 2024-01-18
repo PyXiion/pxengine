040896251a0188a7fefbd85987e2fa8fe9b317d6
PyXiion <me@pyxiion.ru>
2024-01-18 13:06:51 +0300

    Refactor and fixes

--- src/px/script/function.cpp
+++ src/px/script/function.cpp
@@ -8,29 +8,28 @@
 
 namespace px::script {
   priv::FunctionHandle::FunctionHandle(asIScriptContext *context, asIScriptFunction *function, asIScriptObject *self)
-      : m_ctx(context)
-      , m_func(function)
-      , m_self(self) {
-    if (m_ctx)  m_ctx->AddRef();
-    if (m_func) m_func->AddRef();
-    if (m_self) m_self->AddRef();
+    : m_ctx(context)
+  , m_func(function)
+  , m_self(self) {
+    if (m_ctx) { m_ctx->AddRef(); }
+    if (m_func) { m_func->AddRef(); }
+    if (m_self) { m_self->AddRef(); }
   }
 
   priv::FunctionHandle::~FunctionHandle() {
-    if (m_self) m_self->Release();
-    if (m_func) m_func->Release();
-    if (m_ctx) m_ctx->Release();
+    if (m_self) { m_self->Release(); }
+    if (m_func) { m_func->Release(); }
+    if (m_ctx) { m_ctx->Release(); }
   }
 
-  priv::FunctionHandle::FunctionHandle(const priv::FunctionHandle &other)
-      : FunctionHandle(other.m_ctx, other.m_func) {
-  }
+  priv::FunctionHandle::FunctionHandle(const FunctionHandle &other)
+    : FunctionHandle(other.m_ctx, other.m_func, other.m_self) {}
 
-  priv::FunctionHandle &priv::FunctionHandle::operator=(const priv::FunctionHandle &other) {
+  priv::FunctionHandle &priv::FunctionHandle::operator=(const FunctionHandle &other) {
     if (&other != this) {
       m_self = other.m_self;
       m_func = other.m_func;
-      m_ctx = other.m_ctx;
+      m_ctx  = other.m_ctx;
       if (m_self) m_self->AddRef();
       if (m_ctx) m_ctx->AddRef();
       if (m_func) m_func->AddRef();
@@ -39,19 +38,19 @@ namespace px::script {
   }
 
   priv::FunctionHandle::FunctionHandle(priv::FunctionHandle &&other) noexcept
-      : m_ctx(other.m_ctx)
-      , m_func(other.m_func)
-      , m_self(other.m_self) {
-    other.m_ctx = nullptr;
+    : m_ctx(other.m_ctx)
+  , m_func(other.m_func)
+  , m_self(other.m_self) {
+    other.m_ctx  = nullptr;
     other.m_func = nullptr;
     other.m_self = nullptr;
   }
 
   priv::FunctionHandle &priv::FunctionHandle::operator=(priv::FunctionHandle &&other) noexcept {
     if (&other != this) {
-      m_ctx = other.m_ctx;
-      m_func = other.m_func;
-      other.m_ctx = nullptr;
+      m_ctx        = other.m_ctx;
+      m_func       = other.m_func;
+      other.m_ctx  = nullptr;
       other.m_func = nullptr;
     }
     return *this;
@@ -68,35 +67,38 @@ namespace px::script {
   void priv::FunctionHandle::prepare() {
     check();
     m_ctx->Prepare(m_func);
-    CVLOG(2, "AngelScript") << "Preparing context " << m_ctx << " with function " << m_func;
+    CVLOG(9, "AngelScript") << "Preparing context " << m_ctx << " with function " << m_func;
 
     if (m_self) {
-      CVLOG(2, "AngelScript") << "\tSelf: " << m_self;
+      CVLOG(9, "AngelScript") << "\tSelf: " << m_self;
       m_ctx->SetObject(m_self);
     }
   }
 
   void priv::FunctionHandle::setArg(int arg, std::int8_t value) {
     check();
-    CVLOG(2, "AngelScript") << "\tArg[" << arg << "] int8 " << value << " (" << static_cast<std::uint8_t>(value) << ")";
+    CVLOG(9, "AngelScript") << "\tArg[" << arg << "] int8 " << value << " (" << static_cast<std::uint8_t>(value) << ")";
     m_ctx->SetArgByte(arg, value);
   }
 
   void priv::FunctionHandle::setArg(int arg, std::int16_t value) {
     check();
-    CVLOG(2, "AngelScript") << "\tArg[" << arg << "] int16 " << value << " (" << static_cast<std::uint16_t>(value) << ")";
+    CVLOG(9, "AngelScript") << "\tArg[" << arg << "] int16 " << value << " (" << static_cast<std::uint16_t>(value) <<
+ ")";
     m_ctx->SetArgWord(arg, value);
   }
 
   void priv::FunctionHandle::setArg(int arg, std::int32_t value) {
     check();
-    CVLOG(2, "AngelScript") << "\tArg[" << arg << "] int32 " << value << " (" << static_cast<std::uint32_t>(value) << ")";
+    CVLOG(9, "AngelScript") << "\tArg[" << arg << "] int32 " << value << " (" << static_cast<std::uint32_t>(value) <<
+ ")";
     m_ctx->SetArgDWord(arg, value);
   }
 
   void priv::FunctionHandle::setArg(int arg, std::int64_t value) {
     check();
-    CVLOG(2, "AngelScript") << "\tArg[" << arg << "] int64 " << value << " (" << static_cast<std::uint64_t>(value) << ")";
+    CVLOG(9, "AngelScript") << "\tArg[" << arg << "] int64 " << value << " (" << static_cast<std::uint64_t>(value) <<
+ ")";
     m_ctx->SetArgQWord(arg, value);
   }
 
@@ -108,32 +110,32 @@ namespace px::script {
 
   void priv::FunctionHandle::setArg(int arg, double value) {
     check();
-    CVLOG(2, "AngelScript") << "\tArg[" << arg << "] double " << value;
+    CVLOG(9, "AngelScript") << "\tArg[" << arg << "] double " << value;
     m_ctx->SetArgDouble(arg, value);
   }
 
   void priv::FunctionHandle::setArgAddress(int arg, void **ptr) {
     check();
-    CVLOG(2, "AngelScript") << "\tArg[" << arg << "] address " << ptr;
+    CVLOG(9, "AngelScript") << "\tArg[" << arg << "] address " << ptr;
     m_ctx->SetArgAddress(arg, ptr);
   }
 
   void priv::FunctionHandle::setArgObject(int arg, void *ptr) {
     check();
-    CVLOG(2, "AngelScript") << "\tArg[" << arg << "] object " << ptr;
+    CVLOG(9, "AngelScript") << "\tArg[" << arg << "] object " << ptr;
     m_ctx->SetArgObject(arg, ptr);
   }
 
   int priv::FunctionHandle::execute() {
     check();
-    CVLOG(2, "AngelScript") << "Executing context " << m_ctx;
+    CVLOG(9, "AngelScript") << "Executing context " << m_ctx;
     return m_ctx->Execute();
   }
 
   template<>
   std::int8_t priv::FunctionHandle::getReturn() {
     check();
-//    CVLOG(2, "AngelScript") << "\tGot byte return " << m_ctx;
+    //    CVLOG(2, "AngelScript") << "\tGot byte return " << m_ctx;
     return m_ctx->GetReturnByte();
   }
 
@@ -192,5 +194,4 @@ namespace px::script {
       throw std::runtime_error("Calling a function with nullptr context");
     }
   }
-
 } // px::script
